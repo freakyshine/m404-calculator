b43f47c8c92f911cbf16f7b8ca46fea2f267bbbb
freakyshine <p.daniel@medalu.ch>
2020-11-08 16:41:42 +0100

    Calculator taking input with nice prompt message and validates inputs first

--- M404Calculator/Calculate.cs
+++ M404Calculator/Calculate.cs
@@ -1,29 +1,67 @@
-﻿namespace M404Calculator
+﻿using System;
+
+namespace M404Calculator
 {
     class Calculate
     {
 
-        private int cachedResult;
-
-        public int Addition (int number1, int number2)
+        private int cachedResult; // this is the result of the last calculation
+        private int number1;
+        private int number2;
+        private string userIn; // This variables used for the number input and stores an temporary value as string - it will be parsed to an integer
+        public int Addition ()
         {
+            DefineNumbers(" + "); 
+
             cachedResult = number1 + number2;
             return cachedResult;
         }
-        public int Subtraction (int number1, int number2)
+        public int Subtraction ()
         {
+            DefineNumbers(" - ");
+
             cachedResult = number1 - number2;
             return cachedResult;
         }
-        public int Multiplication (int number1, int number2)
+        public int Multiplication ()
         {
+            DefineNumbers(" * ");
+
             cachedResult = number1 * number2;
             return cachedResult;
         }
 
-        public int Division (int number1, int number2)
+        public int Division ()
         {
+            DefineNumbers(" / ");
+
             cachedResult = number1 / number2;
+            return cachedResult;
+        }
+
+        /*
+         This function defines the variables number1 and number 2 and uses the given strings for the command prompts
+        */
+        private void DefineNumbers(string calcSign)
+        {
+            do // While the userinput cannot be parsed it continuosly asks for an input
+            {
+                Console.WriteLine("Please enter an integer:");
+                userIn = Console.ReadLine();
+            } while (!int.TryParse(userIn, out number1));
+
+            do // While the userinput cannot be parsed it continuosly asks for an input
+            {
+                Console.Clear();
+                Console.Write(number1 + calcSign);
+                userIn = Console.ReadLine();
+            } while (!int.TryParse(userIn, out number2));
+        }
+
+        public int GetResult ()
+        {
+            Console.Clear();
+            Console.Write("The result is: ");
             return cachedResult;
         }
     }
--- M404Calculator/Program.cs
+++ M404Calculator/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Globalization;
 using System.Transactions;
 
 namespace M404Calculator
@@ -7,17 +8,43 @@ namespace M404Calculator
     {
         static void Main(string[] args)
         {
+            string input;
             Calculate calculator = new Calculate();
-
-            Console.WriteLine("Please enter 2 numbers and enter them seperated by a line break");
-            Console.WriteLine("Addition: "+calculator.Addition(Convert.ToInt32(Console.ReadLine()), Convert.ToInt32(Console.ReadLine())));
-            Console.WriteLine("Please enter 2 numbers and enter them seperated by a line break"); 
-            Console.WriteLine("Subtraction: " + calculator.Subtraction(Convert.ToInt32(Console.ReadLine()), Convert.ToInt32(Console.ReadLine())));
-            Console.WriteLine("Please enter 2 numbers and enter them seperated by a line break");
-            Console.WriteLine("Multiplication: " + calculator.Multiplication(Convert.ToInt32(Console.ReadLine()), Convert.ToInt32(Console.ReadLine())));
-            Console.WriteLine("Please enter 2 numbers and enter them seperated by a line break");
-            Console.WriteLine("Division: " + calculator.Division(Convert.ToInt32(Console.ReadLine()), Convert.ToInt32(Console.ReadLine())));
-
+            do {
+                Console.WriteLine("Please enter a command \n" +
+                "Enter [1] for Addition, [2] for Subtraction, [3] for Multiplication, [4] for Division, [99] to exit");
+                input = Console.ReadLine();
+                Console.Clear();
+                switch (input)
+                {
+                    case "1":
+                        Console.WriteLine("You've chosen 1 for addition");
+                        calculator.Addition();
+                        Console.WriteLine(calculator.GetResult());
+                        break;
+                    case "2":
+                        Console.WriteLine("You've chosen 2 for subtraction");
+                        calculator.Subtraction();
+                        Console.WriteLine(calculator.GetResult());
+                        break;
+                    case "3":
+                        Console.WriteLine("You've chosen 3 for multiplication");
+                        calculator.Multiplication();
+                        Console.WriteLine(calculator.GetResult());
+                        break;
+                    case "4":
+                        Console.WriteLine("You've chosen 4 for division");
+                        calculator.Division();
+                        Console.WriteLine(calculator.GetResult());
+                        break;
+                }
+                if (input != "99" || input != "") // this enables the user to see the result instead of just going to the menu back again
+                {
+                    Console.WriteLine("Please enter any key to continue.");
+                    Console.ReadKey();
+                }
+                Console.Clear();
+            } while (input != "99");
         }
     }
 }
